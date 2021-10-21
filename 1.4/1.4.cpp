a771ccba17d1e4752140fb10a244e88e00cc0bbc
Riley Houghton <HougRile352@GoTVDSB.ca>
2021-10-21 11:10:02 -0400

    Add project files.

---
+++ 1.4/1.4.cpp
@@ -0,0 +1,43 @@
+// 1.4.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+using namespace std;
+#include <iostream>
+
+int main()
+{
+    const float price = 2;
+    float itemsbought;
+    float taxlessprice;
+    float tax;
+    float finalprice;
+    float paid;
+    float change;
+
+    cout << "How many items are you going to purchase?\n";
+    cin >> itemsbought;
+
+    taxlessprice = itemsbought * price;
+    tax = taxlessprice * .13;
+    finalprice = taxlessprice + tax;
+
+    cout << "Price without tax: " << taxlessprice << "\n";
+    cout << "Tax: " << tax << "\n";
+    cout << "Your total is: " << finalprice << "\n";
+
+    cout << "How much are you going to pay?\n";
+    cin >> paid;
+
+    change = paid - finalprice;
+    cout << "Your change is: " << change;
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
